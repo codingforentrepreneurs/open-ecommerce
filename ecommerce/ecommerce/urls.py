cf78a301991405a858dd320693dbcdd391f154d0
Justin Mitchel <codingforentrepreneurs@gmail.com>
2014-07-02 14:48:52 -0700

    Lecture: Context in Templates

--- ecommerce/ecommerce/urls.py
+++ ecommerce/ecommerce/urls.py
@@ -5,7 +5,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', 'products.views.abc', name='home'),
+    url(r'^$', 'products.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
--- ecommerce/products/views.py
+++ ecommerce/products/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def abc(request):
-
-	template = 'home.html'
-	context = locals()
+def home(request):
+	if request.user.is_authenticated():
+		username_is = "Justin using context"
+		context = {"username_is": request.user}
+	else:
+		context = {"username_is": request.user}
+	
+	template = 'base.html'	
 	return render(request, template, context)

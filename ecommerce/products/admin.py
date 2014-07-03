cf3b4d413d919655563a1ae71e31c7825c6354a6
Justin Mitchel <codingforentrepreneurs@gmail.com>
2014-07-02 17:35:47 -0700

    First Model

--- ecommerce/products/admin.py
+++ ecommerce/products/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Product
+
+
+admin.site.register(Product)
--- ecommerce/products/models.py
+++ ecommerce/products/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+	title = models.CharField(max_length=120)
+	description = models.TextField(null=True, blank=True)
+	price = models.DecimalField(decimal_places=2, max_digits=100, default=29.99)
+	slug = models.SlugField()
+	timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+	updated = models.DateTimeField(auto_now_add=False, auto_now=True)
+	active = models.BooleanField(default=True)
+	
+	def __unicode__(self):
+		return self.title

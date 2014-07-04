39af1997726bfc6534a026b84714110d9dab3b37
Justin Mitchel <codingforentrepreneurs@gmail.com>
2014-07-03 18:13:43 -0700

    Static Files

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -82,6 +82,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static", "media")
+#MEDIA_ROOT = '/Users/jmitch/Desktop/ecommerce/static/media/'
+
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static", "static_root")
+
+STATICFILES_DIRS = (
+    os.path.join(os.path.dirname(BASE_DIR), "static", "static_files"),
+)
 
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR, 'templates'),
--- ecommerce/ecommerce/urls.py
+++ ecommerce/ecommerce/urls.py
@@ -1,5 +1,6 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
-
+from django.conf.urls.static import static
 from django.contrib import admin
 admin.autodiscover()
 
@@ -10,4 +11,9 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-)
+) 
+
+
+if settings.DEBUG:
+	urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

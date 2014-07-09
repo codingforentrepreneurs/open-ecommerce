9c3145a6e5dfbb2d9954cfb99e77eb0ed6decc8b
Justin Mitchel <codingforentrepreneurs@gmail.com>
2014-07-08 17:57:50 -0700

    Cart Count in Navbar

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -78,6 +78,16 @@ USE_L10N = True
 
 USE_TZ = True
 
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.contrib.auth.context_processors.auth",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.core.context_processors.request",
+    "django.core.context_processors.static",
+    "django.core.context_processors.tz",
+    "django.contrib.messages.context_processors.messages",
+)
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/

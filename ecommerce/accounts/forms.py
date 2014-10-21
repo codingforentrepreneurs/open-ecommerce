cab6168fcbd9812c3d378175e4d8215757081337
Justin Mitchel <codingforentrepreneurs@gmail.com>
2014-10-21 11:53:35 -0700

    Form Validation

--- ecommerce/accounts/forms.py
+++ ecommerce/accounts/forms.py
@@ -1,6 +1,32 @@
 from django import forms
 
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class LoginForm(forms.Form):
 	username = forms.CharField()
 	password = forms.CharField(widget=forms.PasswordInput())
+
+	def clean_username(self):
+		username = self.cleaned_data.get("username")
+		try:
+			user = User.objects.get(username=username)
+		except User.DoesNotExist:
+			raise forms.ValidationError("Are you sure you are registered? We cannot find this user.")
+		return username
+
+	def clean_password(self):
+		username = self.cleaned_data.get("username")
+		password = self.cleaned_data.get("password")
+		try:
+			user = User.objects.get(username=username)
+		except:
+			user = None
+		if user is not None and not user.check_password(password):
+			raise forms.ValidationError("Invalid Password")
+		elif user is None:
+			pass
+		else:
+			return password
+			

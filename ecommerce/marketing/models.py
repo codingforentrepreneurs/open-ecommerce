6fb9cb371993c3e67369bd81ab0d7bbc507aa9bb
Justin Mitchel <codingforentrepreneurs@gmail.com>
2014-10-29 14:56:02 -0700

    Message Display Duration

--- ecommerce/marketing/models.py
+++ ecommerce/marketing/models.py
@@ -1,18 +1,22 @@
+import datetime
+
 from django.db import models
 
 # Create your models here.
 
-class MarketingMessageQueryset(models.query.QuerySet):
+class MarketingQueryset(models.query.QuerySet):
 	def active(self):
 		return self.filter(active=True)
 
 	def featured(self):
-		return self.filter(featured=True)
+		return self.filter(featured=True)\
+			.filter(start_date__lt=datetime.datetime.now())\
+			.filter(end_date__gte=datetime.datetime.now())
 
 
-class MarketingMessageManager(models.Manager):
+class MarketingManager(models.Manager):
 	def get_queryset(self):
-		return MarketingMessageQueryset(self.model, using=self._db)
+		return MarketingQueryset(self.model, using=self._db)
 
 	def all(self):
 		return self.get_queryset().active()
@@ -22,7 +26,7 @@ class MarketingMessageManager(models.Manager):
 
 	def get_featured_item(self):
 		try:
-			return self.get_queryset().active().featured().order_by("-start_date")[0]
+			return self.get_queryset().active().featured()[0]
 		except:
 			return None
 
@@ -35,11 +39,14 @@ class MarketingMessage(models.Model):
 	start_date = models.DateTimeField(auto_now_add=False, auto_now=False, null=True, blank=True)
 	end_date = models.DateTimeField(auto_now_add=False, auto_now=False, null=True, blank=True)
 
-	objects = MarketingMessageManager()
-	#messages = MarketingMessageManager()
+	objects = MarketingManager()
 
 	def __unicode__(self):
 		return str(self.message[:12])
+
+	class Meta:
+		ordering = ['-start_date', '-end_date']
+
 
 
 

f71fbed116cc14dfbbd83d4cac224a078d66f9c6
Justin Mitchel <codingforentrepreneurs@gmail.com>
2014-11-01 16:55:30 -0700

    Ajax Email Sign Up

--- ecommerce/marketing/views.py
+++ ecommerce/marketing/views.py
@@ -3,6 +3,7 @@ import datetime
 
 from django.conf import settings
 from django.shortcuts import render, HttpResponse, Http404
+from django.http import HttpResponseBadRequest
 from django.utils import timezone
 
 from .forms import EmailForm
@@ -24,13 +25,15 @@ def dismiss_marketing_message(request):
 
 def email_signup(request):
 	if request.method == "POST":
+		print request.POST
 		form = EmailForm(request.POST)
 		if form.is_valid():
-			email= form.cleaned_data['email']
+			email = form.cleaned_data['email']
+			request.session['email_added_marketing'] = True
 			return HttpResponse('Success %s' %(email))
 		if form.errors:
 			print form.errors
 			json_data = json.dumps(form.errors)
-			return HttpResponse(json_data, content_type='application/json')
+			return HttpResponseBadRequest(json_data, content_type='application/json')
 	else:
 		raise Http404
